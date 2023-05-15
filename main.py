9f5d00ab79b92c60e454823664f3b7319eedee58
andyching168 <andyching168@gmail.com>
2023-05-15 21:31:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import openai
 import requests
 import json
@@ -39,12 +41,11 @@ openai.api_key = OpenAI_APIKEY
 
 def process_gpt_response(text):
     # 分割回答，得到API指令和裝置名稱
-    api_command, device_name = text.strip().split("][")
+    api_command, device_name = text.split("]")
     api_command = api_command.split("[")
     Chat_Response = api_command[0]
-    device_name = device_name.split("]")
 
-    return Chat_Response,api_command[1], device_name[0]
+    return Chat_Response,api_command[1]
 def call_home_assistant(api_command,device):
     url = HA_URL+"/api/services/" + api_command
     headers = {
@@ -70,22 +71,27 @@ while(1):
   response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-          {"role": "system", "content": "現在你是一個可以用指令控制我家裡開關和燈的GPT，叫你HomeGPT好了。具體而言，像是以下對話:\nUSER:請你幫我打開大燈\nHomeGPT:沒問題，正在打開大燈。[light/turn_on][大燈]\n---\nUSER:請你幫我關掉風扇\nHomeGPT:沒問題，正在關掉風扇。[fan/turn_off][風扇]\n---\nUSER:請你幫我關掉螢幕開關\nHomeGPT:沒問題，正在關掉螢幕開關。[switch/turn_off][螢幕開關]"},
+          {"role": "system", "content": "現在你是一個可以用指令控制我家裡開關和燈的GPT，叫你HomeGPT好了。你有可能接收到用戶的各種自然語言指令 ，而你在前面可以用你的方式回答用戶，而在後面框住的地方需要用固定格式輸出 。具體而言，像是以下對話:\nUSER:請你幫我打開書房大燈\nHomeGPT:沒問題，正在打開書房大燈。[打開書房大燈]\n---\nUSER:請你幫我關掉書房的所有風扇\nHomeGPT:沒問題，正在關掉風扇。[關掉書房的風扇]\n---\nUSER:請你幫我關掉螢幕開關\nHomeGPT:沒問題，正在關掉螢幕開關。[關掉螢幕開關]\n注意:在打開或關掉一個區域內所有東西時，不要用[所有]，用[的]，像是你想用[關掉書房所有燈]就要轉成[關掉書房的燈]，另外，當你不清楚的話，請在最後輸出[error]"},
           {"role": "user", "content": question},
       ]
   )
   assistant_answer = response['choices'][0]['message']['content']
-
-  chat_response,api_command, device_name = process_gpt_response(assistant_answer)
-  print(chat_response)
-  #print(f"API指令: {api_command}")
-  #print(f"裝置名稱: {device_name}")
-
-
-  device_id=get_device_id(device_name)
-  api_command
-  home_assistant_response = call_home_assistant(api_command,device_id)
-
-  #待實作:控制多個裝置，預計是讓ChatGPT產生多組請求，再用Python判斷是單次請求還是多次請求
-  #單次:沒問題，正在關掉大燈。[Single][1][light/turn_off][大燈]
-  #多次:沒問題，正在關掉大燈和風扇。[Multi][2][light/turn_off][大燈][fan/turn_off][風扇]
+  #print(assistant_answer)
+  if "error" in assistant_answer:
+    print("ChatGPT Error")
+  else:
+    chat_response,api_command = process_gpt_response(assistant_answer)
+    print(chat_response)
+    #print(f"API指令: {api_command}")
+    #print(f"裝置名稱: {device_name}")
+    HA_Result=call_home_assistant_control(api_command)
+    #print(HA_Result)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+              {"role": "system", "content": "現在你是一個可以用指令控制我家裡開關和燈的GPT，叫你HomeGPT好了。剛才用戶問你:"+question+"\n，然後你用Home Assistant的API處理之後返回的結果是:"+HA_Result+"請用簡短的方式告訴用戶資訊"},
+              {"role": "user", "content": question},
+          ]
+    )
+    assistant_answer = response['choices'][0]['message']['content']
+    print(assistant_answer)
