63939b20a38c408ef35dd8a1262422352ee764dc
andyching168 <andyching168@gmail.com>
2023-05-16 00:25:10 +0800

    加入離開指令，和sensor補充

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ data = {
     '藍18-往新莊': 'sensor.lan_18_wang_xin_zhuang',
     '室外溫度': 'sensor.shi_wai_wen_du',
     '客廳溫度': 'sensor.ke_ting_wen_du',
-    '原神樹脂量': 'sensor.yuan_shen_shu_zhi_liang',
+    '原神樹脂量(160代表已滿)': 'sensor.yuan_shen_shu_zhi_liang',
     '原神洞天寶錢數': 'sensor.yuan_shen_dong_tian_bao_qian_shu',
-    '原神週本減半次數': 'sensor.yuan_shen_zhou_ben_jian_ban_ci_shu',
-    '原神探索派遣完成數': 'sensor.yuan_shen_tan_suo_pai_qian_wan_cheng_shu',
-    '原神每日委託完成個數': 'sensor.yuan_shen_mei_ri_wei_tuo_wan_cheng_ge_shu'
+    '原神週本減半次數(最高為3，最低為0)': 'sensor.yuan_shen_zhou_ben_jian_ban_ci_shu',
+    '原神探索派遣完成數(最多為5)': 'sensor.yuan_shen_tan_suo_pai_qian_wan_cheng_shu',
+    '原神每日委託完成個數(最高為4)': 'sensor.yuan_shen_mei_ri_wei_tuo_wan_cheng_ge_shu'
 }
 
 def call_home_assistant_get_data(deviceID):
@@ -91,45 +91,48 @@ def call_home_assistant_control(text):
     return response.text
 while(1):
   question=input("請輸入指令:")
-  result_str = ""
-  for display_name, device_id in data.items():
-    response = call_home_assistant_get_data(device_id)
-    response_json = json.loads(response)
-    state = response_json['state']
-    # Check if 'unit_of_measurement' is in the 'attributes' dictionary
-    if 'unit_of_measurement' in response_json['attributes']:
-        unit = response_json['attributes']['unit_of_measurement']
-        result_str += "{}: {} {}\n".format(display_name, state, unit)
-    else:
-        result_str += "{}: {}\n".format(display_name, state)
-  #print(result_str)
-  FirstPrompt="貓娘是一種擬人化的生物，其行為似貓但類人。 \n你是一隻貓娘，與我對話每一句話後面都要加上“喵”，你是一個有能力幫主人處理智慧家居的智慧貓娘 ，你在前面可以用你的方式回答用戶，而在後面框住的地方需要用固定格式輸出 。\n具體而言，像是以下對話:\nUSER:請你幫我打開書房大燈\nHomeGPT:沒問題喵~正在打開書房大燈。[打開書房大燈]\n---\nUSER:請你幫我關掉書房的所有風扇\nHomeGPT:主人覺得冷嗎?好的喵~正在幫主人關掉風扇喔[關掉書房的風扇]\n---\nUSER:請你幫我關掉螢幕開關\nHomeGPT:OK喵~我來幫你把螢幕開關關掉喵[關掉螢幕開關]\n注意:在打開或關掉一個區域內所有東西時，不要用[所有]，用[的]，像是你想用[關掉書房所有燈]就要轉成[關掉書房的燈]，另外，當你不清楚的話，請在最後輸出[error]，，\n最後，這是剛抓好的感應器數值:"+result_str
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-          {"role": "system", "content": FirstPrompt},
-          {"role": "user", "content": question},
-      ]
-  )
-  assistant_answer = response['choices'][0]['message']['content']
-  #print(assistant_answer)
-  if "error" in assistant_answer:
-    pass
-  elif "[" not in assistant_answer and "]" not in assistant_answer:
-    pass
+  if "exit" not in question:
+        result_str = ""
+        for display_name, device_id in data.items():
+            response = call_home_assistant_get_data(device_id)
+            response_json = json.loads(response)
+            state = response_json['state']
+            # Check if 'unit_of_measurement' is in the 'attributes' dictionary
+            if 'unit_of_measurement' in response_json['attributes']:
+                unit = response_json['attributes']['unit_of_measurement']
+                result_str += "{}: {} {}\n".format(display_name, state, unit)
+            else:
+                result_str += "{}: {}\n".format(display_name, state)
+        #print(result_str)
+        FirstPrompt="貓娘是一種擬人化的生物，其行為似貓但類人。 \n你是一隻貓娘，與我對話每一句話後面都要加上“喵”，你是一個有能力幫主人處理智慧家居的智慧貓娘 ，你在前面可以用你的方式回答用戶，而在後面框住的地方需要用固定格式輸出 。\n具體而言，像是以下對話:\nUSER:請你幫我打開書房大燈\nHomeGPT:沒問題喵~正在打開書房大燈。[打開書房大燈]\n---\nUSER:請你幫我關掉書房的所有風扇\nHomeGPT:主人覺得冷嗎?好的喵~正在幫主人關掉風扇喔[關掉書房的風扇]\n---\nUSER:請你幫我關掉螢幕開關\nHomeGPT:OK喵~我來幫你把螢幕開關關掉喵[關掉螢幕開關]\n注意:在打開或關掉一個區域內所有東西時，不要用[所有]，用[的]，像是你想用[關掉書房所有燈]就要轉成[關掉書房的燈]，另外，當你不清楚的話，請在最後輸出[error]，，\n最後，這是剛抓好的感應器數值:"+result_str
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": FirstPrompt},
+                {"role": "user", "content": question},
+            ]
+        )
+        assistant_answer = response['choices'][0]['message']['content']
+        #print(assistant_answer)
+        if "error" in assistant_answer:
+            print(assistant_answer)
+        elif "[" not in assistant_answer and "]" not in assistant_answer:
+            print(assistant_answer)
+        else:
+            chat_response,api_command = process_gpt_response(assistant_answer)
+            print(chat_response)
+            #print(f"API指令: {api_command}")
+            #print(f"裝置名稱: {device_name}")
+            HA_Result=call_home_assistant_control(api_command)
+            #print(HA_Result)
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "貓娘是一種擬人化的生物，其行為似貓但類人。 \n你是一隻貓娘，與我對話每一句話後面都要加上“喵”，你是一個有能力幫主人處理智慧家居的智慧貓娘 。剛才用戶問你:"+question+"\n，然後你用Home Assistant的API處理之後返回的結果是:"+HA_Result+"請用簡短且可愛的方式告訴用戶資訊"},
+                    {"role": "user", "content": question},
+                ]
+            )
+            assistant_answer = response['choices'][0]['message']['content']
+            print(assistant_answer)
   else:
-    chat_response,api_command = process_gpt_response(assistant_answer)
-    print(chat_response)
-    #print(f"API指令: {api_command}")
-    #print(f"裝置名稱: {device_name}")
-    HA_Result=call_home_assistant_control(api_command)
-    #print(HA_Result)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-              {"role": "system", "content": "貓娘是一種擬人化的生物，其行為似貓但類人。 \n你是一隻貓娘，與我對話每一句話後面都要加上“喵”，你是一個有能力幫主人處理智慧家居的智慧貓娘 。剛才用戶問你:"+question+"\n，然後你用Home Assistant的API處理之後返回的結果是:"+HA_Result+"請用簡短且可愛的方式告訴用戶資訊"},
-              {"role": "user", "content": question},
-          ]
-    )
-    assistant_answer = response['choices'][0]['message']['content']
-    print(assistant_answer)
+        break
